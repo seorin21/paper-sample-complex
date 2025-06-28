161fe44d669159b2c07f52b4bf282204c8e28351
seorin21 <seorin021@gmail.com>
2025-06-29 03:40:01 +0900

    🔨 Maven 배포 최종
    다만 '-publish'도 같이 배포되는 문제가 있습니다.
    
    다른 모듈을 사용하면 해결되나, 필요성을 느끼지 못하여 변경하지 않았습니다.

--- sample-publish/build.gradle.kts
+++ sample-publish/build.gradle.kts
@@ -14,19 +14,19 @@ publishing {
     }
 
     publications {
+        fun MavenPublication.setup(target: Project) {
+            artifactId = target.name
+            from(target.components["java"])
+            artifact(target.tasks["sourcesJar"])
+            artifact(target.tasks["dokkaJar"])
+        }
+
         create<MavenPublication>("api") {
-            artifactId = projectApi.name
-            from(projectApi.components["java"])
-            // 위에서 생성한 'sourcesJar', 'dokkaJar' 작업을 아티팩트로 추가합니다.
-            artifact(projectApi.tasks.named("sourcesJar", Jar::class.java))
-            artifact(projectApi.tasks.named("dokkaJar", Jar::class.java))
+            setup(projectApi)
         }
 
         create<MavenPublication>("core") {
-            artifactId = projectCore.name
-            from(projectCore.components["java"])
-            artifact(projectCore.tasks.named("sourcesJar", Jar::class.java))
-            artifact(projectCore.tasks.named("dokkaJar", Jar::class.java))
+            setup(projectCore)
             artifact(coreReobfJar)
         }
     }
@@ -34,9 +34,8 @@ publishing {
 
 mavenPublishing {
     pom {
-        name.set(rootProject.name)
-        description.set("유튜브 \"서린\"의 컨텐츠 서버 '잔혹한 추위'를 위한, 의존성 라이브러리입니다.")
-        url.set("https://github.com/mulgyeolpyo/${rootProject.name}")
+        description.set("유튜브 \"서린\"의 모듈입니다!")
+        url.set("https://github.com/seorin21/${rootProject.name}")
 
         licenses {
             license {
@@ -57,9 +56,9 @@ mavenPublishing {
         }
 
         scm {
-            connection.set("scm:git:git://github.com/mulgyeolpyo/${rootProject.name}.git")
-            developerConnection.set("scm:git:ssh://github.com:mulgyeolpyo/${rootProject.name}.git")
-            url.set("https://github.com/mulgyeolpyo/${rootProject.name}")
+            connection.set("scm:git:git://github.com/seorin21/${rootProject.name}.git")
+            developerConnection.set("scm:git:ssh://github.com:seorin21/${rootProject.name}.git")
+            url.set("https://github.com/seorin21/${rootProject.name}")
         }
     }
 
